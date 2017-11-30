ca3698130265c3a900fbcec6c0ade1a38d66de55
Benjamin Kowatsch <bk077@hdm-stuttgart.de>
2017-11-30 23:27:06 +0100

    Updated docker-compose files

--- InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/activties/MainActivity.java
+++ InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/activties/MainActivity.java
@@ -1,7 +1,10 @@
 package com.media.interactive.cs3.hdm.interactivemedia.activties;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -9,10 +12,61 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.policy.Condition;
+import com.amazonaws.auth.policy.Policy;
+import com.amazonaws.auth.policy.Principal;
+import com.amazonaws.auth.policy.Resource;
+import com.amazonaws.auth.policy.Statement;
+import com.amazonaws.auth.policy.actions.S3Actions;
+import com.amazonaws.mobileconnectors.s3.transfermanager.Download;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3EncryptionClient;
+import com.amazonaws.services.s3.model.BucketPolicy;
+import com.amazonaws.services.s3.model.EncryptionMaterials;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListVersionsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.S3VersionSummary;
+import com.amazonaws.services.s3.model.SetBucketPolicyRequest;
+import com.amazonaws.services.s3.model.StaticEncryptionMaterialsProvider;
+import com.amazonaws.services.s3.model.VersionListing;
+import com.media.interactive.cs3.hdm.interactivemedia.ObjectStorage;
 import com.media.interactive.cs3.hdm.interactivemedia.R;
 import com.media.interactive.cs3.hdm.interactivemedia.contentprovider.DatabaseHelper;
 import com.media.interactive.cs3.hdm.interactivemedia.data.Hash;
 import com.media.interactive.cs3.hdm.interactivemedia.data.Login;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.NoSuchAlgorithmException;
+import java.util.Iterator;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +82,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         databaseHelper = new DatabaseHelper(this);
 
+
+        try {
+            new ObjectStorage().execute(this,"Test.png").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
 /*
         databaseHelper.deleteAllUsers();
         databaseHelper.resetDatabase();
@@ -38,7 +101,6 @@ public class MainActivity extends AppCompatActivity {
         testUser.setHashedPassword(Hash.hashStringSHA256("Passwort1234"));
         databaseHelper.deleteAllUsers();
 */
-
         Login.getInstance().clear();
 
         appTag = getResources().getString(R.string.app_tag);
@@ -47,6 +109,8 @@ public class MainActivity extends AppCompatActivity {
 
         Log.e(TAG,"Exceptionally Thread Id: "+android.os.Process.getThreadPriority(android.os.Process.myTid()));
     }
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void launchNextActivity() {
@@ -78,6 +142,8 @@ public class MainActivity extends AppCompatActivity {
                    finish();
                 });
     }
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
