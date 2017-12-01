a1135bfb01b9f0c3c1425678913343a1b10047db
Maximilian Bischoff <maximilianbischoff@web.de>
2017-12-01 18:41:33 +0100

    refactor: added user for groups-test

--- backend/testing-api/test/groups/groups.js
+++ backend/testing-api/test/groups/groups.js
@@ -7,23 +7,39 @@ chai.use(require('chai-http'));
 
 const host = 'http://backend:8081';
 const baseUrl = '/v1/groups';
+const loginUrl = '/v1/users/';
 
-describe('Get version', function() {
+const testData = require('../data/users');
+
+describe('Get groups', function() {
     this.timeout(5000); // How long to wait for a response (ms)
+    var defaultToken;
+    before(function(done) {
+        // first register a new default user
+        chai.request(host)
+            .post(loginUrl)
+            .send({username: testData.users.valid[2].username, password: testData.users.valid[2].password})
+            .then(function(res) {
+                defaultToken = res.body.payload.accessToken;
+              })
+            // then request a valid access token from facebook
+            .catch((error) => {
+            console.log('Facbook Login Error: ' + error);
+            done();
+          });
+      });
 
-    before(function() {});
-    after(function() {});
-
-    // GET - Register new user
-    it('should get current app version', function() {
+    it('should respond with 403 if all groups are accessed as nonAdmin', function() {
         return chai.request(host)
             .get(baseUrl + '/')
+            .send({'accessToken': defaultToken, 'authType': 0})
             .then(function(res) {
-                expect(res).to.have.status(404);
+                expect(res).to.have.status(403);
                 expect(res).to.be.json;
                 expect(res.body).to.be.an('object');
-                expect(res.body.name).to.equal('Backend');
-                expect(res.body.version).to.have.lengthOf.above(0);
+                expect(res.body.payload).to.equal('Admin access required');
+                expect(res.body.success).to.be.false;
               });
       });
-  });
+  })
+;
