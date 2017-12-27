ae878e814cbcd719e52b7121b4a0cd3dd1f3a210
Pirmin Rehm <pr057@hdm-stuttgart.de>
2017-12-27 22:24:32 +0100

    BE: refactor: group.module: split big promise chains in many  functions
    * export logic into small reusable functions
    * the promise chains are now much more better readable

--- backend/modules/group.module.js
+++ backend/modules/group.module.js
@@ -27,55 +27,16 @@ module.exports.createNewGroup = function(creatorId, groupData) {
     groupData.groupId = uuidService.generateUUID();
     groupData.createdAt = new Date();
     groupData.transactions = [];
-    let errorToReturn = {isSelfProvided: true};
     let groupUserObjects;
 
-    // generate array of promises of db calls for find user by id
-    let groupUserPromises = [];
-    for (let i = 0; i < groupData.users.length; i++) {
-      groupUserPromises.push(database.collections.users.findOne({email: groupData.users[i]}));
-    }
-
-    Promise.all(groupUserPromises).then(findOneValues => {
-      // return all groupUserIds if object is not null
-      let groupUserIds = findOneValues.map(val => val ? val.userId : null);
-      let groupUserEmails = findOneValues.map(val => val ? val.email : null);
-      groupUserObjects = findOneValues.map(val => val ? {userId: val.userId, username: val.username} : null);
-      errorToReturn.dataPath = 'groupUsers';
-      errorToReturn.errorCode = ERROR.INVALID_CREATE_GROUP_VALUES;
-
-      // checken if an id is null => unknonw user
-      if (groupUserIds.includes(null)) {
-        //filter returns true, if email of groupData.users is not in groupUserEmails
-        let diffEmails = groupData.users.filter(i => groupUserEmails.indexOf(i) < 0).toString();
-        errorToReturn.message = 'Unknown user: ' + diffEmails;
-        errorToReturn.errorCode = ERROR.UNKNOWN_USER;
-        return Promise.reject(errorToReturn);
-      //check if groupUsers contains groupCreator by creatorId
-      } else if (!groupUserIds.includes(creatorId)) {
-        errorToReturn.message = 'GroupCreator must be part of groupUsers';
-        return Promise.reject(errorToReturn);
-      // check for duplicated groupUsers by filter (-> has length > 0 if duplicated entries)
-      } else if (groupUserIds.filter((val, i, self) => self.indexOf(val) !== i).length) {
-        errorToReturn.message = 'Duplicated groupUsers';
-        return Promise.reject(errorToReturn);
-      // no error -> replace emails with ids and insert into db
-      } else {
-        groupData.users = groupUserIds;
-        return database.collections.groups.insertOne(groupData);
-      }
-    }).then(result => {
-      let userUpdatePromises = [];
-      let update = {
-        '$push': {
-          'groupIds': groupData.groupId
-        }
-      };
-      for (let i = 0; i < groupData.users.length; i++) {
-        userUpdatePromises.push(database.collections.users.updateOne({userId: groupData.users[i]}, update));
-      }
-      return Promise.all(userUpdatePromises);
-    }).then(result => {
+    findUsersByField('email', groupData.users)
+    .then(findUsersResult => checkForInvalideCreateGroupValues(findUsersResult, groupData.users, creatorId))
+    .then(groupUser => {
+      groupData.users = groupUser.ids;
+      groupUserObjects = groupUser.objects;
+      return database.collections.groups.insertOne(groupData);
+    }).then(() => addGroupIdToUsers(groupData.groupId, groupData.users))
+    .then(result => {
       winston.debug('Creating a new group successful');
       groupData.users = groupUserObjects; //returns also the username
       responseData.payload = groupData;
@@ -104,41 +65,18 @@ module.exports.getGroupById = function(groupId) {
   winston.debug('Hello from module getGroupById');
   return new Promise((resolve, reject) => {
     let responseData = {payload: {}};
-    let errorToReturn = {isSelfProvided: true};
-    Promise.resolve().then(() => {
-      if (!groupId) {
-        errorToReturn.message = 'missing groupId in URL';
-        errorToReturn.errorCode = ERROR.MISSING_ID_IN_URL;
-        return Promise.reject(errorToReturn);
-      } else {
-        let query = {groupId: groupId};
-        let options = {fields: {_id: false}};
-        return database.collections.groups.findOne(query, options);
-      }
-    }).then(groupResult => {
-      if (!groupResult) {
-        errorToReturn.message = 'group not found';
-        errorToReturn.errorCode = ERROR.UNKNOWN_GROUP;
-        return Promise.reject(errorToReturn);
-      } else {
-        responseData.payload = groupResult;
-        let groupUserPromises = [];
-        for (let i = 0; i < groupResult.users.length; i++) {
-          groupUserPromises.push(database.collections.users.findOne({userId: groupResult.users[i]}));
-        }
-        return Promise.all(groupUserPromises);
-      }
-    }).then(userResults => {
-      if (userResults.includes(null)) {
-        errorToReturn.message = 'Unknown user in group';
-        errorToReturn.errorCode = ERROR.UNKNOWN_USER;
-        return Promise.reject(errorToReturn);
-      } else {
-        let groupUserObjects = userResults.map(val => ({userId: val.userId, username: val.username}));
-        responseData.payload.users = groupUserObjects;
-        responseData.success = true;
-        resolve(responseData);
-      }
+    checkIfGroupIdIsGiven(groupId)
+    .then(findGroupById)
+    .then(checkForGroupResult)
+    .then(groupResult => {
+      responseData.payload = groupResult;
+      return findUsersByField('userId', groupResult.users);
+    }).then(checkUserResults)
+    .then(userResults => {
+      let groupUserObjects = userResults.map(val => ({userId: val.userId, username: val.username}));
+      responseData.payload.users = groupUserObjects;
+      responseData.success = true;
+      resolve(responseData);
     }).catch(err => {
       winston.debug(err);
       responseData.success = false;
@@ -159,30 +97,12 @@ module.exports.getGroupById = function(groupId) {
 module.exports.verifyGroupContainsUser = function(userId, groupId) {
   return new Promise((resolve, reject) => {
     let errorToReturn = {isSelfProvided: true};
-    Promise.resolve().then(()=> {
-      if (!groupId) {
-        errorToReturn.message = 'missing groupId in URL';
-        errorToReturn.errorCode = ERROR.MISSING_ID_IN_URL;
-        return Promise.reject(errorToReturn);
-      } else {
-        let query = {groupId: groupId};
-        let options = {fields: {objectId: true, users: true}};
-        return database.collections.groups.findOne(query, options);
-      }
-    }).then(groupResult => {
-      if (!groupResult) {
-        errorToReturn.dataPath = 'group';
-        errorToReturn.message = 'group not found';
-        errorToReturn.errorCode = ERROR.UNKNOWN_GROUP;
-        return Promise.reject(errorToReturn);
-      } else if (groupResult.users.indexOf(userId) < 0) {
-        errorToReturn.dataPath = 'authorization';
-        errorToReturn.message = 'user is not but has to be a member of the group';
-        errorToReturn.errorCode = ERROR.USER_NOT_IN_GROUP;
-        return Promise.reject(errorToReturn);
-      } else {
-        resolve(groupResult);
-      }
+    checkIfGroupIdIsGiven(groupId)
+    .then(findGroupById)
+    .then(checkForGroupResult)
+    .then(groupResult => checkIfUserIdIsInGroup(groupResult, userId))
+    .then(groupResult => {
+      resolve(groupResult);
     }).catch(err => {
       winston.debug(err);
       let responseData = {payload: {dataPath: 'authorization'}, success: false};
@@ -200,3 +120,106 @@ module.exports.verifyGroupContainsUser = function(userId, groupId) {
   });
 };
 
+function checkForInvalideCreateGroupValues(findUsersResult, requestedUserEmails, creatorId) {
+  let errorToReturn = {isSelfProvided: true};
+  errorToReturn.dataPath = 'groupUsers';
+  errorToReturn.errorCode = ERROR.INVALID_CREATE_GROUP_VALUES;
+  // return all groupUserIds if object is not null
+  let groupUserIds = findUsersResult.map(val => val ? val.userId : null);
+  let groupUserEmails = findUsersResult.map(val => val ? val.email : null);
+  let groupUserObjects = findUsersResult.map(val => val ? {userId: val.userId, username: val.username} : null);
+  // checken if an id is null => unknonw user
+  if (groupUserIds.includes(null)) {
+    //filter returns true, if email of groupData.users is not in groupUserEmails
+    let diffEmails = requestedUserEmails.filter(i => groupUserEmails.indexOf(i) < 0).toString();
+    errorToReturn.message = 'Unknown user: ' + diffEmails;
+    errorToReturn.errorCode = ERROR.UNKNOWN_USER;
+    return Promise.reject(errorToReturn);
+  //check if groupUsers contains groupCreator by creatorId
+  } else if (!groupUserIds.includes(creatorId)) {
+    errorToReturn.message = 'GroupCreator must be part of groupUsers';
+    return Promise.reject(errorToReturn);
+  // check for duplicated groupUsers by filter (-> has length > 0 if duplicated entries)
+  } else if (groupUserIds.filter((val, i, self) => self.indexOf(val) !== i).length) {
+    errorToReturn.message = 'Duplicated groupUsers';
+    return Promise.reject(errorToReturn);
+  // no error -> replace emails with ids and insert into db
+  } else {
+    return {objects: groupUserObjects, ids: groupUserIds};
+  }
+}
+
+function findUsersByField(field, users) {
+  // generate array of promises of db calls for find user by id
+  let groupUserPromises = [];
+  for (let i = 0; i < users.length; i++) {
+    groupUserPromises.push(database.collections.users.findOne({[field]: users[i]}));
+  }
+  return Promise.all(groupUserPromises);
+}
+
+function addGroupIdToUsers(groupId, userIds) {
+  let userUpdatePromises = [];
+  let update = {
+    '$push': {
+      'groupIds': groupId
+    }
+  };
+  for (let i = 0; i < userIds.length; i++) {
+    userUpdatePromises.push(database.collections.users.updateOne({userId: userIds[i]}, update));
+  }
+  return Promise.all(userUpdatePromises);
+}
+
+function findGroupById(groupId) {
+  let query = {groupId: groupId};
+  let options = {fields: {_id: false}};
+  return database.collections.groups.findOne(query, options);
+}
+
+function checkIfGroupIdIsGiven(groupId) {
+  if (!groupId) {
+    let errorToReturn = {isSelfProvided: true};
+    errorToReturn.dataPath = 'group';
+    errorToReturn.message = 'missing groupId in URL';
+    errorToReturn.errorCode = ERROR.MISSING_ID_IN_URL;
+    return Promise.reject(errorToReturn);
+  } else {
+    return Promise.resolve(groupId); // must resolve -> groupId is not an object
+  }
+}
+
+function checkForGroupResult(groupResult) {
+  if (!groupResult) {
+    let errorToReturn = {isSelfProvided: true};
+    errorToReturn.dataPath = 'group';
+    errorToReturn.message = 'group not found';
+    errorToReturn.errorCode = ERROR.UNKNOWN_GROUP;
+    return Promise.reject(errorToReturn);
+  } else {
+    return groupResult;
+  }
+}
+
+function checkUserResults(userResults) {
+  if (userResults.includes(null)) {
+    let errorToReturn = {isSelfProvided: true};
+    errorToReturn.message = 'Unknown user in group';
+    errorToReturn.errorCode = ERROR.UNKNOWN_USER;
+    return Promise.reject(errorToReturn);
+  } else {
+    return userResults;
+  }
+}
+
+function checkIfUserIdIsInGroup(groupResult, userId) {
+  if (groupResult.users.indexOf(userId) < 0) {
+    let errorToReturn = {isSelfProvided: true};
+    errorToReturn.dataPath = 'authorization';
+    errorToReturn.message = 'user is not but has to be a member of the group';
+    errorToReturn.errorCode = ERROR.USER_NOT_IN_GROUP;
+    return Promise.reject(errorToReturn);
+  } else {
+    return groupResult;
+  }
+}
