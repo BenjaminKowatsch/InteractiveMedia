5b874b7f9c5aeba06647ff692365ac89c4fe01e7
Alexander Wallrabenstein <alexander.wallrabenstein@gmail.com>
2017-12-28 20:26:25 +0100

    BE: add utility route /admin/add to activate admin after resetting db

---
+++ backend/controllers/admin.controller.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const winston = require('winston');
+const config = require('../modules/config');
+const user = require('../modules/user.module');
+const httpResponseService = require('../services/httpResponse.service');
+const ERROR = require('../config.error');
+const ROLES = require('../config.roles');
+
+module.exports.addAdmin = function(req, res) {
+    let responseData = {payload: {}};
+    user.register(config.adminUsername, config.adminPassword, config.adminEmail, ROLES.ADMIN).then(registerResult => {
+        winston.info('register admin successful');
+        responseData.success = true;
+        responseData.payload.accessToken = registerResult.payload.accessToken;
+        responseData.payload.authType = registerResult.payload.authType;
+        httpResponseService.send(res, 201, responseData);
+      }).catch(errorResult => {
+        winston.error(JSON.stringify(errorResult));
+        let statusCode;
+        switch (errorResult.errorCode) {
+          case ERROR.DUPLICATED_USER:
+            // admin already exists
+            statusCode = 422;
+            break;
+          case ERROR.DB_ERROR:
+            statusCode = 500;
+            break;
+        }
+        httpResponseService.send(res, statusCode, errorResult.responseData);
+      });
+  };
