3ca3d64a92b8f6839c8b617e18efea09225ad8b1
Maximilian Bischoff <maximilianbischoff@web.de>
2018-01-06 16:01:30 +0100

    App: Removed unused imports

--- InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/activties/AddTransactionActivity.java
+++ InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/activties/AddTransactionActivity.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EdgeEffect;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
