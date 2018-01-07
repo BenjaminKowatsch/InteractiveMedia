f18427f1b8d2c2930f785aeee2e7142f320202f5
Alexander Wallrabenstein <alexander.wallrabenstein@gmail.com>
2018-01-07 12:01:59 +0100

    FE: fix broken env variables: supply during build + import in webpack
    webpack requires webServiceUrl and jwtSimpleSecret during build them to write them into Config.js

--- frontend/src/js/Config.js
+++ frontend/src/js/Config.js
@@ -1,6 +1,5 @@
 var settings = {
     "jwtSimpleSecret": process.env.JWT_SIMPLE_SECRET,
     "webServiceURL": process.env.WEB_SERVICE_URL,
-    "origin": process.env.ORIGIN_URL
 };
 module.exports = settings;
