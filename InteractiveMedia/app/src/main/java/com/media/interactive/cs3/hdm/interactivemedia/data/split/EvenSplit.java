c0ee8fbfc9ce98aa25cd7100eea8bd33312b0434
Maximilian Bischoff <maximilianbischoff@web.de>
2018-01-13 19:09:29 +0100

    App: Added more methods to Split interface for later usage

--- InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/data/split/EvenSplit.java
+++ InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/data/split/EvenSplit.java
@@ -9,6 +9,17 @@ import java.util.List;
 public class EvenSplit implements Split {
     @Override
     public List<Debt> split(Transaction transaction) {
+
         return null;
+    }
+
+    @Override
+    public Split andThen(Split next) {
+        throw new TerminatingSplitChainedException(this.getClass().getName() + " is always terminating");
+    }
+
+    @Override
+    public boolean isTerminating() {
+        return true;
     }
 }
