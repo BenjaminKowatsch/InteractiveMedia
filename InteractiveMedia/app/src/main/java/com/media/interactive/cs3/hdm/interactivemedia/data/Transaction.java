cf9b3ebb8d20b9831ab8133141659105cd616551
Maximilian Bischoff <maximilianbischoff@web.de>
2018-01-15 14:30:46 +0100

    App: Fixed split method with old signature being called

--- InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/data/Transaction.java
+++ InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/data/Transaction.java
@@ -60,7 +60,7 @@ public class Transaction {
     }
 
     public List<Debt> split() {
-        return SplitFactory.getSplitByName(split, group).split(this, paidByUserId);
+        return SplitFactory.getSplitByName(split).split(this, paidByUserId);
     }
 
     public long getId() {
--- InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/data/split/SplitFactory.java
+++ InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/data/split/SplitFactory.java
@@ -1,14 +1,12 @@
 package com.media.interactive.cs3.hdm.interactivemedia.data.split;
 
 
-import com.media.interactive.cs3.hdm.interactivemedia.data.Group;
-
 public class SplitFactory {
 
-    public static Split getSplitByName(String name, Group group) {
+    public static Split getSplitByName(String name) {
         switch (name.toLowerCase()) {
             case "even":
-                return new EvenSplit(group);
+                return new EvenSplit();
             default:
                 throw new UnknownSplitException("No split named " + name
                         + " known to this factory");
