c948e58bec0d50d0801719c217df9ac524fd4748
Alexander Schübl <as333@hdm-stuttgart.de>
2018-01-15 20:09:40 +0100

    Added dummy AdminProfile page

---
+++ frontend/src/pages/adminProfile.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <div>
+        
+        Blubb
+        
+        <router-link to="/overview" exact>          
+            Go back
+        </router-link>
+    </div>
+          
+</template>
