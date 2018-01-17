c969b9b4e292322506014ab0f7e7ccd2a17f470c
Maximilian Bischoff <maximilianbischoff@web.de>
2018-01-17 15:24:02 +0100

    App: Added helper method for loading debts

--- InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/contentprovider/tables/DebtTable.java
+++ InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/contentprovider/tables/DebtTable.java
@@ -1,6 +1,9 @@
 package com.media.interactive.cs3.hdm.interactivemedia.contentprovider.tables;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import com.media.interactive.cs3.hdm.interactivemedia.data.Debt;
 
 /**
  * Created by benny on 20.11.17.
@@ -35,20 +38,28 @@ public class DebtTable {
     public static final String COLUMN_CREATED_AT = "created_at";
 
     public static final String DATABASE_CREATE =
-        "create table if not exists " + TABLE_NAME + "("
-            + COLUMN_ID + " integer unique primary key AUTOINCREMENT,"
-            + COLUMN_TRANSACTION_ID + " INTEGER references " + TransactionTable.TABLE_NAME
-            + " (" + TransactionTable.COLUMN_ID + ") on delete cascade on update cascade,"
-            + COLUMN_AMOUNT + " REAL NOT NULL,"
-            + COLUMN_FROM_USER + " INTEGER references " + UserTable.TABLE_NAME
-            + " (" + UserTable.COLUMN_ID + ") on delete cascade on update cascade,"
-            + COLUMN_TO_USER + " INTEGER references " + UserTable.TABLE_NAME
-            + " (" + UserTable.COLUMN_ID + ") on delete cascade on update cascade,"
-            + COLUMN_CREATED_AT + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+            "create table if not exists " + TABLE_NAME + "("
+                    + COLUMN_ID + " integer unique primary key AUTOINCREMENT,"
+                    + COLUMN_TRANSACTION_ID + " INTEGER references " + TransactionTable.TABLE_NAME
+                    + " (" + TransactionTable.COLUMN_ID + ") on delete cascade on update cascade,"
+                    + COLUMN_AMOUNT + " REAL NOT NULL,"
+                    + COLUMN_FROM_USER + " INTEGER references " + UserTable.TABLE_NAME
+                    + " (" + UserTable.COLUMN_ID + ") on delete cascade on update cascade,"
+                    + COLUMN_TO_USER + " INTEGER references " + UserTable.TABLE_NAME
+                    + " (" + UserTable.COLUMN_ID + ") on delete cascade on update cascade,"
+                    + COLUMN_CREATED_AT + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
 
     public static final String DATABASE_DROP = "drop table if exists " + TABLE_NAME;
 
     public static void onCreate(SQLiteDatabase database) {
         database.execSQL(DATABASE_CREATE);
+    }
+
+    public static Debt extractDebtFromCurrentPosition(Cursor cursor) {
+        final long transactionId = cursor.getLong(cursor.getColumnIndex(COLUMN_TRANSACTION_ID));
+        final long creditorId = cursor.getLong(cursor.getColumnIndex(COLUMN_TO_USER));
+        final long debtorId = cursor.getLong(cursor.getColumnIndex(COLUMN_FROM_USER));
+        final double amount = cursor.getDouble(cursor.getColumnIndex(COLUMN_AMOUNT));
+        return new Debt(creditorId, debtorId, amount, transactionId);
     }
 }
