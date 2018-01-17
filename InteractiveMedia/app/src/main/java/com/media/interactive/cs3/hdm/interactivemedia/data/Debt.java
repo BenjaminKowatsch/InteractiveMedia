94b555fa82239524ec88b7194c511effad831e11
Maximilian Bischoff <maximilianbischoff@web.de>
2018-01-17 15:25:21 +0100

    App: Refactored debt to only hold id references to user and transaction

--- InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/data/Debt.java
+++ InteractiveMedia/app/src/main/java/com/media/interactive/cs3/hdm/interactivemedia/data/Debt.java
@@ -2,25 +2,38 @@ package com.media.interactive.cs3.hdm.interactivemedia.data;
 
 
 public class Debt {
-    private final User creditor;
-    private final User debtor;
+    private final long creditorId;
+    private final long debtorId;
     private final double amount;
+    private final long transactionId;
 
-    public Debt(User creditor, User debtor, double amount) {
-        this.creditor = creditor;
-        this.debtor = debtor;
+    public Debt(User creditor, User debtor, double amount, Transaction createdFrom) {
+        this.creditorId = creditor.getId();
+        this.debtorId = debtor.getId();
         this.amount = amount;
+        this.transactionId = createdFrom.getId();
     }
 
-    public User getCreditor() {
-        return creditor;
+    public Debt(long creditorId, long debtorId, double amount, long transactionId) {
+        this.creditorId = creditorId;
+        this.debtorId = debtorId;
+        this.amount = amount;
+        this.transactionId = transactionId;
     }
 
-    public User getDebtor() {
-        return debtor;
+    public long getCreditorId() {
+        return creditorId;
+    }
+
+    public long getDebtorId() {
+        return debtorId;
     }
 
     public double getAmount() {
         return amount;
+    }
+
+    public long getTransactionId() {
+        return transactionId;
     }
 }
