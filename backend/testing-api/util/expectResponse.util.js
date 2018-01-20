6c578029645a069059d30572030f8e3dc4933cc2
Alexander Wallrabenstein <alexander.wallrabenstein@gmail.com>
2018-01-20 16:17:05 +0100

    BE: ref: apitest: DRY: apply builder pattern to create ExpectError() obj

--- backend/testing-api/util/expectResponse.util.js
+++ backend/testing-api/util/expectResponse.util.js
@@ -6,142 +6,116 @@ const chai = require('chai');
 const expect = require('chai').expect;
 const winston = require('winston');
 
+const ExpectError = require('./expectError.util');
+
 module.exports.toBe400InvalidRequestBody = function(res) {
-  expect(res).to.have.status(400);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('validation');
-  expect(res.body.payload.message).to.equal('invalid request body');
+  new ExpectError()
+  .withStatusCode(400)
+  .withMessage('invalid request body')
+  .withDataPath('validation')
+  .inResponse(res);
 };
 
 module.exports.toBe401InvalidAuthToken = function(res) {
-  expect(res).to.have.status(401);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('authentication');
-  expect(res.body.payload.message).to.equal('invalid authToken');
+  new ExpectError()
+  .withStatusCode(401)
+  .withDataPath('authentication')
+  .withMessage('invalid authToken')
+  .inResponse(res);
 };
 
 module.exports.toBe401LoginFailed = function(res) {
-  expect(res).to.have.status(401);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('login');
-  expect(res.body.payload.message).to.equal('login failed');
+  new ExpectError()
+  .withStatusCode(401)
+  .withDataPath('login')
+  .withMessage('login failed')
+  .inResponse(res);
 };
 
 module.exports.toBe400InvalidAuthType = function(res) {
-  expect(res).to.have.status(400);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('authType');
-  expect(res.body.payload.message).to.equal('invalid auth type');
+  new ExpectError()
+  .withStatusCode(400)
+  .withDataPath('authType')
+  .withMessage('invalid auth type')
+  .inResponse(res);
 };
 
 module.exports.toBe401MissingHeaderAuthorization = function(res) {
-  expect(res).to.have.status(401);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('authentication');
-  expect(res.body.payload.message).to.equal('missing http request header Authorization');
+  new ExpectError()
+  .withStatusCode(401)
+  .withDataPath('authentication')
+  .withMessage('missing http request header Authorization')
+  .inResponse(res);
 };
 
 module.exports.toBe400CreateGroupDuplicatedUsers = function(res) {
-  expect(res).to.have.status(400);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('groupUsers');
-  expect(res.body.payload.message).to.equal('Duplicated groupUsers');
+  new ExpectError()
+  .withStatusCode(400)
+  .withDataPath('groupUsers')
+  .withMessage('Duplicated groupUsers')
+  .inResponse(res);
 };
 
 module.exports.toBe400CreateGroupMissingCreator = function(res) {
-  expect(res).to.have.status(400);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('groupUsers');
-  expect(res.body.payload.message).to.equal('GroupCreator must be part of groupUsers');
+  new ExpectError()
+  .withStatusCode(400)
+  .withDataPath('groupUsers')
+  .withMessage('GroupCreator must be part of groupUsers')
+  .inResponse(res);
 };
 
 module.exports.toBe409CreateGroupNonExistingUser = function(res, emailOfNotExistingUser) {
-  expect(res).to.have.status(409);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('groupUsers');
-  expect(res.body.payload.message).to.equal('Unknown user: ' + emailOfNotExistingUser);
+  new ExpectError()
+  .withStatusCode(409)
+  .withDataPath('groupUsers')
+  .withMessage('Unknown user: ' + emailOfNotExistingUser)
+  .inResponse(res);
 };
 
 module.exports.toBe401InvalidFormatHeaderAuthorization = function(res) {
-  expect(res).to.have.status(401);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.be.equal('authentication');
-  expect(res.body.payload.message).to.be.equal('invalid format of http request header Authorization');
+  new ExpectError()
+  .withStatusCode(401)
+  .withDataPath('authentication')
+  .withMessage('invalid format of http request header Authorization')
+  .inResponse(res);
 };
 
 module.exports.toBe403GroupsUserIsNotMember = function(res) {
-  expect(res).to.have.status(403);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('authorization');
-  expect(res.body.payload.message).to.equal('user is not but has to be a member of the group');
+  new ExpectError()
+  .withStatusCode(403)
+  .withDataPath('authorization')
+  .withMessage('user is not but has to be a member of the group')
+  .inResponse(res);
 };
 
 module.exports.toBe404GroupNotFound = function(res) {
-  expect(res).to.have.status(404);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('group');
-  expect(res.body.payload.message).to.equal('group not found');
+  new ExpectError()
+  .withStatusCode(404)
+  .withDataPath('group')
+  .withMessage('group not found')
+  .inResponse(res);
 };
 
 module.exports.toBe403Unauthorized = function(res) {
-  expect(res).to.have.status(403);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.be.equal('authorization');
-  expect(res.body.payload.message).to.be.equal('user is not authorized');
+  new ExpectError()
+  .withStatusCode(403)
+  .withDataPath('authorization')
+  .withMessage('user is not authorized')
+  .inResponse(res);
 };
 
 module.exports.toBe404UserNotFound = function(res) {
-  expect(res).to.have.status(404);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('user');
-  expect(res.body.payload.message).to.equal('user not found');
+  new ExpectError()
+  .withStatusCode(404)
+  .withDataPath('user')
+  .withMessage('user not found')
+  .inResponse(res);
 };
 
 module.exports.toBe401UnknownUserOrExpiredToken = function(res) {
-  expect(res).to.have.status(401);
-  expect(res).to.be.json;
-  expect(res.body).to.be.an('object');
-  expect(res.body.success).to.be.false;
-  expect(res.body.payload).to.be.an('object');
-  expect(res.body.payload.dataPath).to.equal('user');
-  expect(res.body.payload.message).to.equal('unknown user or expired token');
+  new ExpectError()
+  .withStatusCode(401)
+  .withDataPath('user')
+  .withMessage('unknown user or expired token')
+  .inResponse(res);
 };
