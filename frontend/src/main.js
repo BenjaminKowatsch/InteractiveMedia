b09e70b4998e884db2c931956e5847188edc5acb
Alexander Schübl <as333@hdm-stuttgart.de>
2018-01-21 23:18:29 +0100

    delete dependencys and imports

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,17 +7,13 @@ import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
 import ('../node_modules/vuetify/dist/vuetify.min.css')
 
-// Import VueTouch for tables
-import VueTouch from 'vue-touch'
-
 // Import VueCharts for charts
 import VueCharts from 'vue-chartjs'
 
 // Import App Custom Styles
 import AppStyles from './css/app.css'
 
-// Import Routes
-import Routes from './routes.js'
+
 
 // Import App Component
 import App from './app'
@@ -36,11 +32,7 @@ import Cookie from './js/Cookie.js'
 import Config from './js/Config.js'
 
 import Mixins from './mixins.js'
-import { TableComponent, TableColumn } from 'vue-table-component';
 
-Vue.component('table-component', TableComponent);
-Vue.component('table-column', TableColumn);
-Vue.use(VueTouch, { name: 'v-touch' });
 Vue.use(VueRouter)
 Vue.use(Vuetify)
 
@@ -56,21 +48,6 @@ var startApp = function(onLoadingFinished) {
 
 var onLoadingFinished = function() {
 
-
-    //ToDO: Try to do the routes into routes.js
-    /*            const routes = [{
-                       path: '/',
-                       name: "login",
-                       component: login
-                   },
-                   {
-                       path: '/overview/',
-                       name: "overview",
-                       component: overview
-                   },
-               ] */
-
-
     const router = new VueRouter({
 
         linkActiveClass: 'active',
@@ -83,10 +60,6 @@ var onLoadingFinished = function() {
             { path: "/noPermission", name: "noPermission", component: noPermission },
             { path: '*', component: notFound },
             { path: '/*/', component: notFound },
-
-
-            // ... other routes ...
-            // and finally the default route, when none of the above matches:
         ]
     })
 
@@ -96,20 +69,6 @@ var onLoadingFinished = function() {
         mixins: [Mixins],
         template: '<app ref="app" ><app/>',
         router,
-        // Init Framework7 by passing parameters here
-        /*         framework7: {
-                    root: '#app',
-                    modalTitle: 'Debts² Admin Panel',
-                    //Uncomment to enable Material theme: 
-                    material: false,
-                   //  Enable browser hash navigation 
-                    pushState: true,
-                   // Set oparator for browser hash navigation 
-                    pushStateSeparator: '#',
-                    pushStateOnLoad: false,
-                    //animatePages : false,
-                    routes: Routes,
-                }, */
         // Register App Component
         components: {
             app: App
@@ -121,8 +80,6 @@ var onLoadingFinished = function() {
         },
         created: function() {
 
-
-            // Only update the loginStatus if the user is not already logged in with facebook
             this.updateLoginStatus();
         },
         mounted: function() {
@@ -131,19 +88,10 @@ var onLoadingFinished = function() {
             if (true === this.loginStatus) {
                 this.redirect("/overview", false, false, false);
             }
-            /*             this.addPushStatePanel();
-             */
+
         },
         methods: {
-            /**
-             * Function adds an event listener to framework7 so if on any page the back button will be clicked the side panel will be closed
-             */
-            /*   addPushStatePanel: function() {
-                  var framework7 = this.$f7;
-                  framework7.onPageBack("*", function(page) {
-                      framework7.closePanel();
-                  });
-              }, */
+
             updateLoginStatus: function() {
                 // Check if cookies exist
                 // If a cookie exists, set the loginStatus to true
@@ -151,8 +99,7 @@ var onLoadingFinished = function() {
                 // and due to the redirect to the login page, you would run into an infinite loop
                 var accessToken = Cookie.getJSONCookie('accessToken')
                 if (accessToken !== '') {
-                    //var authOptions = ["Launometer","Google","Facebook"];
-                    //Hint: if auth doesnt work after removing mocking, maybe authOptions has to be modified again
+
                     var authOptions = "Debts² Admin Panel";
 
                     console.log(authOptions[accessToken.authType] + " Cookie exists. Redirecting ...");
@@ -161,11 +108,6 @@ var onLoadingFinished = function() {
             }
         }
     });
-    // Delete all entries after logout
-    /*     document.getElementById("logoutButton").onclick = function() {
-            vm.$children[0].$refs.loginForm.reset();
-        };
-     */
 };
 
 startApp(onLoadingFinished);
